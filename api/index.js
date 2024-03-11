9cf23b01b6d06889e11848a99cef6064afac2785
usman <mu163953@gmail.com>
2024-03-11 12:35:00 +0500

    connect and run database

--- api/index.js
+++ api/index.js
@@ -1,4 +1,22 @@
 import express from "express"
+import mongoose from "mongoose"
+import dotenv from "dotenv"
+
+dotenv.config()
+mongoose
+    .connect(process.env.MONGO, {
+        useNewUrlParser: true,
+        useUnifiedTopology: true,
+    })
+    .then(() => {
+        console.log("mongoDb connect");
+    })
+    .catch((err) => {
+        console.log(err);
+    });
+
+
+
 const app = express()
 
 
