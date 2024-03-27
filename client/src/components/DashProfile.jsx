57885dc11071d3cf6546beb7f2b9296d96c5e5ee
usman <mu163953@gmail.com>
2024-03-27 11:47:48 +0500

    compete upload post image funcnality

--- client/src/components/DashProfile.jsx
+++ client/src/components/DashProfile.jsx
@@ -149,7 +149,7 @@ const DashProfile = () => {
     <div className="max-w-lg mx-auto p-3 w-full">
       <h1 className="my-7 text-center font-semibold text-3xl">Profile</h1>
       <form className="flex flex-col gap-4" onSubmit={handleSubmit}>
-        <input type="file" accept="image/*" onChange={handleImageChange} ref={filePickerRef} hidden />
+        <input type="file" accept='image/.*' onChange={handleImageChange} ref={filePickerRef} hidden />
         <div className="relative w-32 h-32 self-center cursor-pointer shadow-md overflow-hidden rounded-full"
           onClick={() => filePickerRef.current.click()}
         >
