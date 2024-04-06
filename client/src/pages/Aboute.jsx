41bb424669f2492fb7c47e38a7eb8b08a5704a7a
usman <mu163953@gmail.com>
2024-04-06 12:12:00 +0500

    complete aboute and project pages

--- client/src/pages/Aboute.jsx
+++ client/src/pages/Aboute.jsx
@@ -2,7 +2,24 @@ import React from 'react'
 
 const Aboute = () => {
   return (
-    <div>Aboute</div>
+    <div className=' min-h-screen flex items-center justify-center'>
+      <div className="max-w-2xl p-3 mx-auto text-center">
+        <div className="div">
+          <h1 className='text-3xl font font-semibold text-center my-7'>Aboute Usman's Blog</h1>
+          <div className="text-md text-gray-500 flex flex-col gap-6">
+            <p>
+              Lorem, ipsum dolor sit amet consectetur adipisicing elit. Repellendus excepturi aliquid nulla enim impedit consectetur atque a delectus commodi debitis facere, id explicabo repudiandae voluptatibus! Deleniti, magnam! Nemo, quod quisquam.
+            </p>
+            <p>
+              Lorem, ipsum dolor sit amet consectetur adipisicing elit. Repellendus excepturi aliquid nulla enim impedit consectetur atque a delectus commodi debitis facere, id explicabo repudiandae voluptatibus! Deleniti, magnam! Nemo, quod quisquam.
+            </p>
+            <p>
+              Lorem, ipsum dolor sit amet consectetur adipisicing elit. Repellendus excepturi aliquid nulla enim impedit consectetur atque a delectus commodi debitis facere, id explicabo repudiandae voluptatibus! Deleniti, magnam! Nemo, quod quisquam.
+            </p>
+          </div>
+        </div>
+      </div>
+    </div>
   )
 }
 
