d0981607b4b78dc48c582b721faf3c14067c4361
Muhammad Usman <mu163953@gmaile.com>
2024-08-05 15:19:56 +0500

    Your commit message

--- client/src/firebase.js
+++ client/src/firebase.js
@@ -1,12 +1,13 @@
 import { initializeApp } from "firebase/app";
 
 const firebaseConfig = {
-    apiKey: import.meta.env.VITE_FIRE_BASE_API_KEY,
+    apiKey: "AIzaSyAYRb1bYHZ0ann9k70oIGDAJGyyBgAbi2M",
     authDomain: "mern-blogs-3c7f1.firebaseapp.com",
     projectId: "mern-blogs-3c7f1",
     storageBucket: "mern-blogs-3c7f1.appspot.com",
     messagingSenderId: "91809185454",
     appId: "1:91809185454:web:a44d2906e2a9b8fd29b73f",
-};
+    measurementId: "G-JK54R2VTQ3"
+  };
 
 export const app = initializeApp(firebaseConfig);
